eea772b2d75e987a64074897a43885d1b6857247
Madhumithakannan-23 <madsuka33@gmail.com>
2025-07-31 07:39:17 +0530

    Add files via upload

---
+++ madhumitha/day4/task10.js
@@ -0,0 +1,13 @@
+// Math Functions
+console.log(Math.random());                  // Random number
+console.log(Math.floor(4.7));               // Rounds down to 4
+console.log(Math.ceil(4.3));                // Rounds up to 5
+console.log(Math.max(3, 5, 1));             // 5
+console.log(Math.min(3, 5, 1));             // 1
+
+// String Functions
+let name = "madhumitha";
+console.log(name.toUpperCase());            // "MADHUMITHA"
+console.log(name.slice(0, 5));              // "madhu"
+console.log(name.includes("mitha"));          // true
+console.log(name.replace("madhu", "s"));    // "smitha"
